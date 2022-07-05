5c5921acefe0e15f43018cee01d91ad151832f5b
Taehillah <ishmaelmafole@gmail.com>
2022-07-05 11:14:55 -0700

    Task 0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Write a program that prints _putchar, followed by a new line.
+ *
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	else
+		return (0);
+}
