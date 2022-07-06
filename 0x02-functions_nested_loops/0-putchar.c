bc6c2364583a3ed5f8115d20f38c682dde23aa3e
Ishmael Mafole <42701424+Taehillah@users.noreply.github.com>
2022-07-06 21:25:21 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*
 * Write a program that prints _putchar, followed by a new line.
 * main is the Entry function
 * Return: Always 0 when successful
 */
-int main(void)
+int main(void) //main function return an int 0 success 1 for opp
 {
 char *putter = "_putchar\n";
 while (*putter)
