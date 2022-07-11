a8ea35c523894e6e710410bde39788078a2872d5
Taehillah <ishmaelmafole@gmail.com>
2022-07-11 12:32:03 -0700

    Task 0. 98 Battery st. Ammended

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - a function that takes a pointer to an int as parameter and 
+ * reset_to_98 - a function that takes a pointer to an int as parameter and
  * updates the value it points to to 98
  * with int *n
  * Return Always 0
