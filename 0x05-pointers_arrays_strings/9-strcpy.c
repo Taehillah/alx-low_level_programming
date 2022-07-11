cff1b491c17cfc4a8bdbc172ab355831e360f433
Taehillah <ishmaelmafole@gmail.com>
2022-07-11 14:21:41 -0700

    Updated spaces

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * _strcpy - copies a string pointed to by @src, including the
- * termination of null byte, to a 
+ * termination of null byte, to a
  * buffer pointed to by @dest.
  * @dest: A buffer to copy the string to.
  * @src: The source string to copy.
@@ -11,7 +11,7 @@
 char *_strcpy(char *dest, const char *src)
 {
 int index = 0;
-while (src[index])
+while (src[index]!= '\0')
 {
 dest[index] = src[index];
 index++;
