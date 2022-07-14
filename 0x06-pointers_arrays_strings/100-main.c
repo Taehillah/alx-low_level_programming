91b06132d167e9197f46adff89d4809e48b454ec
Taehillah <ishmaelmafole@gmail.com>
2022-07-14 12:59:50 -0700

    Task 8. rot13

--- 0x06-pointers_arrays_strings/100-main.c
+++ 0x06-pointers_arrays_strings/100-main.c
@@ -27,16 +27,3 @@ int main(void)
     printf("%s", s);
     return (0);
 }
-julien@ubuntu:~/0x06$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-main.c 100-rot13.c -o 100-rot13
-julien@ubuntu:~/0x06$ ./100-rot13 
-EBG13 ("ebgngr ol 13 cynprf", fbzrgvzrf ulcurangrq EBG-13) vf n fvzcyr yrggre fhofgvghgvba pvcure.
-------------------------------------
-EBG13 ("ebgngr ol 13 cynprf", fbzrgvzrf ulcurangrq EBG-13) vf n fvzcyr yrggre fhofgvghgvba pvcure.
-------------------------------------
-ROT13 ("rotate by 13 places", sometimes hyphenated ROT-13) is a simple letter substitution cipher.
-------------------------------------
-ROT13 ("rotate by 13 places", sometimes hyphenated ROT-13) is a simple letter substitution cipher.
-------------------------------------
-EBG13 ("ebgngr ol 13 cynprf", fbzrgvzrf ulcurangrq EBG-13) vf n fvzcyr yrggre fhofgvghgvba pvcure.
-------------------------------------
-EBG13 ("ebgngr ol 13 cynprf", fbzrgvzrf ulcurangrq EBG-13) vf n fvzcyr yrggre fhofgvghgvba pvcure.
