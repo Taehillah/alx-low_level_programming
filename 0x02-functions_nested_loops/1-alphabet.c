c63b2307ca2225639e24e592581faa275d8673b0
Taehillah <ishmaelmafole@gmail.com>
2022-07-16 15:24:07 -0700

    Task 1. I sometimes suffer from insomnia. And when I can't fall asleep,

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * A function that prints the alphabet, in lowercase, followed by a new line.
- *
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
  */
 void print_alphabet(void)
 {
