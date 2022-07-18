a4195acf155086f6b35f912cc9f0f4485c6400cd
Taehillah <ishmaelmafole@gmail.com>
2022-07-18 13:44:44 -0700

    Task 2. strchr

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,7 +8,7 @@
  * @accept: input
  * Return: Always 0 (Success)
  */
-unsigned int _strspn(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept)
 {
 unsigned int i, n, value, check;
 value = 0;
