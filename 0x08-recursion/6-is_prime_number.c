32784ab9b5a37100d3b79d5f62021e489062657d
Taehillah <ishmaelmafole@gmail.com>
2022-07-21 19:57:29 -0700

    Fixed errors

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,7 @@
+#include "main.h"
 int find_strlen(char *s);
 int check_palindrome(char *s, int len, int index);
 int is_palindrome(char *s);
-
 /**
  * find_strlen - Returns the length of a string.
  * @s: The string to be measured.
@@ -10,15 +10,13 @@ int is_palindrome(char *s);
  */
 int find_strlen(char *s)
 {
-	int len = 0;
-
-	if (*(s + len))
-	{
-		len++;
-		len += find_strlen(s + len);
-	}
-
-	return (len);
+int len = 0;
+if (*(s + len))
+{
+len++;
+len += find_strlen(s + len);
+}
+return (len);
 }
 
 /**
