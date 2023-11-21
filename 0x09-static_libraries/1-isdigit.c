ab9f776179135f505792d7ec9a3ddac3f30a01af
Ishmael Lehlohonolo Mafole <ishmaelmafole@gmail.com>
2023-11-21 22:51:44 +0200

    1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isdigit - checks for a digit between 0 and 9.
- * @c: provided variables to a function.
+ * @c: provided variables to a func.
  * Return: 1 if c is a digit, return 0 oterwise.
  */
 int _isdigit(int c)
